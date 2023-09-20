3fbdd60990e5407f20be61eabe129ad41187be41
Théo m <>
2023-09-20 14:34:55 +0200

    feat : create demo page with datetime

---
+++ src/Controller/DemoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoController extends AbstractController
+{
+    #[Route('/demo', name: 'app_demo')]
+    public function index(): Response
+    {
+        $date = new \DateTime();
+        $formattedDate = $date->format('Y-m-d H:i:s');
+
+        return $this->render('demo/index.html.twig', [
+            'controller_name' => 'DemoController',
+            'date' => $formattedDate,
+        ]);
+    }
+}
